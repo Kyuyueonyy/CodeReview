f8c31b5312b1c7745051a277f2d08488ebd99377
kyuyueonyy <rbddl3470@naver.com>
2025-03-28 17:48:14 +0900

    250328 수업 코드 추가(slot-test)

---
+++ 07_컴포넌트심화/slot-test/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import NoSlotTest from './components/NoSlotTest.vue';
+export default {
+  name: 'App',
+  components: { NoSlotTest },
+};
+</script>
+
+<template>
+  <div>
+    <NoSlotTest />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 07_컴포넌트심화/slot-test/src/components/CheckBox1.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'CheckBox1',
+  props: ['id', 'checked', 'label'],
+};
+</script>
+
+<template>
+  <div>
+    <!-- 부모-> 자식 props로 데이터 전달하는것 -->
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="$emit('check-changed', { id, checked: $event.target.checked })"
+    />
+    <span
+      v-if="checked === true"
+      style="color: blue; text-decoration: underline"
+    >
+      <i>{{ label }}</i>
+    </span>
+    <span v-else style="color: gray">{{ label }}</span>
+  </div>
+</template>
