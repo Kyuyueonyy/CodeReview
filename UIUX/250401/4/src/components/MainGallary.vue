eeac7af13bc4c97a67268ff6fabbacdd2c987263
kyuyueonyy <rbddl3470@naver.com>
2025-04-01 17:46:49 +0900

    250401 UI/UX 특강 수업 코드

---
+++ UIUX/250401/4/src/components/MainGallary.vue
@@ -0,0 +1,30 @@
+<script setup>
+import BaseButton from './UI/BaseButton.vue'
+</script>
+
+<template>
+  <!-- 그리드 컨테이너 -->
+  <main class="component-grid">
+    <!-- 컴포넌트 그룹 (그리드 아이템) -->
+    <section class="component-card">
+      <h2>버튼</h2>
+      <BaseButton variant="btn-primary">Primary</BaseButton>
+      <BaseButton variant="btn-outline">Outline</BaseButton>
+      <BaseButton variant="btn-pill">Pill</BaseButton>
+    </section>
+    <section class="component-card">
+      <h2>알림창</h2>
+      <BaseAlert variant="success">Success</BaseAlert>
+      <BaseAlert variant="warning">Warning</BaseAlert>
+      <BaseAlert variant="error">Error</BaseAlert>
+    </section>
+
+    <section class="component-card">
+      <h2>뱃지</h2>
+      <BaseBadge variant="success">badge</BaseBadge>
+      <BaseBadge variant="number">3</BaseBadge>
+    </section>
+  </main>
+</template>
+
+<style scoped></style>
